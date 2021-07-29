9ca69656e63e22ba1ecb3ca7afdfb15c40f0d6e7
Kaiwen Song <jirachiji@gmail.com>
2021-07-29 00:34:18 -0400

    Stylistic changes to main dashboard and product displays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import ProductDisplay from './components/ProductDisplay.js'
 import NavbarHead from './components/NavbarHead.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,12 +13,18 @@ function App() {
     <div>
       <NavbarHead/>
       <Container>
-        <div class = "all-products">
-          <ProductDisplay img = {pla} name = "Black Filament" qty = "3"/>
-          <ProductDisplay img = {pla} name = "Black Filament" qty = "3"/>
-          <ProductDisplay img = {pla} name = "Black Filament" qty = "3"/>
-          <ProductDisplay img = {pla} name = "Black Filament" qty = "3"/>
-        </div>
+        <Row className = "mt-4">
+          <Col md={3}><ProductDisplay img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+          <Col md={3}><ProductDisplay img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+          <Col md={3}><ProductDisplay img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+          <Col md={3}><ProductDisplay  img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+        </Row>
+        <Row className = "mt-4">
+          <Col md={3}><ProductDisplay img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+          <Col md={3}><ProductDisplay img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+          <Col md={3}><ProductDisplay img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+          <Col md={3}><ProductDisplay  img = {pla} name = "Black Filament" qty = "3" date = "07/31/2020"/></Col>
+        </Row>
       </Container>
     </div>
   );
--- my-app/src/components/ProductDisplay.js
+++ my-app/src/components/ProductDisplay.js
@@ -8,8 +8,9 @@ export default class ProductDisplay extends React.Component {
     return (
       <div class = "product-display">
         <img src = {this.props.img}/>
-        <h3>{this.props.name}</h3>
-        <h3>Qty: {this.props.qty}</h3>
+        <h3 class = "text-center">{this.props.name}</h3>
+        <p>Qty: {this.props.qty}</p>
+        <p>Buy by: {this.props.qty}</p>
       </div>
     )
   }
